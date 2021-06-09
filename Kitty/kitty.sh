d7a33799da01b42966769c89734b27f0aea5276e
Hec98 <hector982015@gmail.com>
2021-06-09 17:02:00 -0500

    Kitty conf

--- Kitty/kitty.sh
+++ Kitty/kitty.sh
@@ -1,2 +1,14 @@
-mkdir ~/.config/kitty &&
-cp -v kitty.conf ~/.config/kitty 
+DIRECTORY=~/.config/kitty
+if [ -d $DIRECTORY ]
+then
+	echo -e "Remove kitty"
+	rm -vr $DIRECTORY
+	mkdir $DIRECTORY
+	echo -e "Copying kitty.conf"
+	cp -v Themes/Gruvbox/kitty.conf $DIRECTORY
+else
+	echo -e "Creating kitty folder"
+	mkdir $DIRECTORY
+	echo -e "Copying kitty.conf"
+	cp -v Themes/Gruvbox/kitty.conf $DIRECTORY
+fi
