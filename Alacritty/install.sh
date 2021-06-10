6e06b2a46063ebb992d6a5e546c7ab21398337c7
Hec98 <hector982015@gmail.com>
2021-06-10 11:29:33 -0500

    Alacritty install

---
+++ Alacritty/install.sh
@@ -0,0 +1,14 @@
+DIRECTORY=~/.config/alacritty
+if [ -d $DIRECTORY ]
+then
+	echo -e "Remove alacritty"
+	rm -vr $DIRECTORY
+	mkdir $DIRECTORY
+	echo -e "Copying alacritty.conf"
+	cp -v alacritty.yml $DIRECTORY
+else
+	echo -e "Creating alacritty folder"
+	mkdir $DIRECTORY
+	echo -e "Copying alacritty.conf"
+	cp -v alacritty.yml $DIRECTORY
+fi
