9ea4f9935065ccd8eba44e7c5ef039c0e2cc5d45
hecdelatorre <hector982015@gmail.com>
2022-07-02 16:08:13 -0500

    Change in Debian files

---
+++ Debian/install-packages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt install arj audacious build-essential cabextract clang cups-pdf deluge filezilla fonts-firacode g++ gcc gcp git git gnome-disk-utility gparted kitty lhasa lzip lzma lzop mpack neofetch nmap nodejs npm openvpn p7zip p7zip-full python3 python3-pip python3-tk rofi sharutils smplayer tree unace w3m zsh -y
