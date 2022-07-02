175597b0912bd0795ab425d7b85096b2ae4c2843
hecdelatorre <hector982015@gmail.com>
2022-07-02 18:01:00 -0500

    Add -> sudo apt autoremove -y

--- Debian/install-packages.sh
+++ Debian/install-packages.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo apt update && sudo apt install arj audacious build-essential cabextract clang cups-pdf deluge filezilla fonts-firacode g++ gcc gcp git git gnome-disk-utility gparted kitty lhasa lzip lzma lzop mpack neofetch nmap nodejs npm openvpn p7zip p7zip-full python3 python3-pip python3-tk rofi sharutils smplayer tree unace w3m zsh -y
+sudo apt autoremove -y
--- Debian/upgrade.sh
+++ Debian/upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
