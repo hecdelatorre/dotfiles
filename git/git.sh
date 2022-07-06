9b3f38827e49f6a6752a44647e5353768b5c2ae1
hecdelatorre <hector982015@gmail.com>
2022-07-05 20:27:31 -0500

    Change in instructions

--- git/git.sh
+++ git/git.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 echo 'Copy configuration'
-echo "[user]                                                                                                                                                    main
-        name = Name
-        email = email@email.com
-[alias]
-        lg = log --graph --abbrev-commit --decorate --format=format:'%C(bold green)%h%C(reset) :: %C(white)%s%C(reset) %C(dim white) -> %an%C(reset)%C(bold yellow)%d%C(reset)' --all
-        s = status -s -b
-[credential]
-        helper = cache --timeout=3600
-[core]
-        editor = nvim
-[github]
-        user = UserName" > .gitcon && mv -f .gitcon ~/.gitconfig
+curl -fsSLo .gitconfig_tem https://gitlab.com/hecdelatorre/dotfiles/-/raw/main/git/.gitconfig
+mv -f .gitconfig_tem ~/.gitconfig
 cat ~/.gitconfig
 echo 'Please edit your file with your parameters -> # nano ~/.gitconfig'
-
