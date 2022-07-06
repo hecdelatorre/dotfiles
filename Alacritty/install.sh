82b39aa91cd28d9e496ab06711094eba43a20cef
hecdelatorre <hector982015@gmail.com>
2022-07-05 19:38:39 -0500

    Change in instructions

--- Alacritty/install.sh
+++ Alacritty/install.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+curl -fsSLo .alacritty.yml https://gitlab.com/hecdelatorre/dotfiles/-/raw/main/Alacritty/alacritty.yml
 DIRECTORY=~/.config/alacritty
+CONFIG=~/.config
+if ! [ -d $CONFIG ]
+then
+	mkdir $CONFIG
+fi
 if [ -d $DIRECTORY ]
 then
 	echo -e "Remove alacritty"
 	rm -vr $DIRECTORY
 	mkdir $DIRECTORY
 	echo -e "Copying alacritty.conf"
-	cp -v alacritty.yml $DIRECTORY
+	mv -vf .alacritty.yml $DIRECTORY/alacritty.yml
 else
 	echo -e "Creating alacritty folder"
 	mkdir $DIRECTORY
 	echo -e "Copying alacritty.conf"
-	cp -v alacritty.yml $DIRECTORY
+	mv -vf .alacritty.yml $DIRECTORY/alacritty.yml
 fi
