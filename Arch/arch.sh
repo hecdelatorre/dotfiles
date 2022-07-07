e21679bfb041a23b497bd2f52e026668b3aab2f3
hecdelatorre <hector982015@gmail.com>
2022-07-07 16:51:20 -0500

    Add Arch/install-fonts.sh

--- Arch/arch.sh
+++ Arch/arch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo pacman -Sy
-sudo pacman -S adobe-source-code-pro-fonts arj audacious base-devel cabextract cantarell-fonts curl deluge-gtk engrampa git gnome-disk-utility gvfs-smb kitty lhasa lzip lzop neovim nodejs-lts-gallium noto-fonts npm ntp openvpn p7zip pulseaudio python-pip python3 rofi sharutils smbnetfs smplayer tk ttf-baekmuk ttf-cascadia-code ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-hanazono ttf-roboto ttf-roboto-mono ttf-ubuntu-font-family unace unrar wget xclip zsh --noconfirm --needed
+sudo pacman -S arj audacious base-devel cabextract cantarell-fonts curl deluge-gtk engrampa git gnome-disk-utility gvfs-smb kitty lhasa lzip lzop neovim nodejs-lts-gallium npm ntp openvpn p7zip pulseaudio python-pip python3 rofi sharutils smbnetfs smplayer tk  unace unrar wget xclip zsh --noconfirm --needed
---
+++ Arch/install-fonts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo pacman -S base-devel git --noconfirm --needed
+sudo pacman -S adobe-source-code-pro-fonts noto-fonts ttf-baekmuk ttf-cascadia-code ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-font-awesome ttf-hanazono ttf-ibm-plex ttf-opensans ttf-roboto ttf-roboto-mono ttf-ubuntu-font-family --noconfirm --needed
+git clone https://aur.archlinux.org/atkinson-hyperlegible-fonts.git
+git clone https://aur.archlinux.org/montserrat-font.git
+git clone https://aur.archlinux.org/ttf-ms-win11.git
+git clone https://aur.archlinux.org/ttf-poppins.git
+git clone https://aur.archlinux.org/ttf-rubik.git 
+git clone https://aur.archlinux.org/ttf-spacemono.git
+git clone https://aur.archlinux.org/nerd-fonts-git.git
+cd atkinson-hyperlegible-fonts && makepkg -si --noconfirm --needed && cd ..
+cd montserrat-font && makepkg -si --noconfirm --needed && cd ..
+cd ttf-ms-win10 && makepkg -si --noconfirm --needed && cd ..
+cd ttf-poppins && makepkg -si --noconfirm --needed && cd ..
+cd ttf-rubik && makepkg -si --noconfirm --needed && cd ..
+cd ttf-spacemono && makepkg -si --noconfirm --needed && cd ..
+cd nerd-fonts-git && makepkg -si --noconfirm --needed && cd ..
+rm -vrf atkinson-hyperlegible-fonts montserrat-font nerd-fonts-git ttf-ms-win11 ttf-poppins ttf-rubik ttf-spacemono
