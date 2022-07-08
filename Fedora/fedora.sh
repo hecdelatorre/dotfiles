5ff5529a0d03aa282c1ae7d6471730653336f626
hecdelatorre <hector982015@gmail.com>
2022-07-07 19:21:18 -0500

    Edit fonts

--- Fedora/fedora.sh
+++ Fedora/fedora.sh
@@ -4,5 +4,3 @@ sudo dnf in https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$
 sudo dnf check-update
 echo -e "\n Install Apps\n"
 sudo dnf in aria2 arj audacious cabextract deluge discord git glances gnome-disk-utility gvfs-smb kitty libreoffice-calc libreoffice-draw libreoffice-gtk3 libreoffice-impress libreoffice-langpack-es libreoffice-writer lzip lzop mpv mpv-libs neofetch neovim numix* openvpn nodejs npm p7zip rofi sharutils unace unrar unrar zsh -y
-
-
