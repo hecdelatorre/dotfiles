dfc7b4f91fc632facc333d398bf3a77a0af6355e
hecdelatorre <hector982015@gmail.com>
2022-07-28 22:52:48 -0500

    Edit file

--- bspwm/install-bspwm.sh
+++ bspwm/install-bspwm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#curl -fsSLo .kitty.conf https://gitlab.com/hecdelatorre/dotfiles/-/raw/main/Kitty/Themes/Kibble/kitty.conf
+curl -fsSLo .bspwmrc https://gitlab.com/hecdelatorre/dotfiles/-/raw/main/bspwm/bspwmrc
+curl -fsSLo .sxhkdrc https://gitlab.com/hecdelatorre/dotfiles/-/raw/main/bspwm/sxhkdrc
+curl -fsSLo .config.ini https://gitlab.com/hecdelatorre/dotfiles/-/raw/main/bspwm/config.ini
+curl -fsSLo .launch.sh https://gitlab.com/hecdelatorre/dotfiles/-/raw/main/bspwm/launch.sh
 
 DIRECTORY_BSPWM=~/.config/bspwm
 DIRECTORY_SXHKD=~/.config/sxhkd
@@ -8,42 +11,44 @@ CONFIG=~/.config
 
 if ! [ -d $CONFIG ]
 then
-	mkdir $CONFIG
+	      mkdir $CONFIG
 fi
 
 if !  [ -d $DIRECTORY_BSPWM ]
 then
-	mkdir $DIRECTORY_BSPWM
-        cp -vrf bspwmrc $DIRECTORY_BSPWM/bspwmrc
-  	chmod +x $HOME/.config/bspwm/bspwmrc
+	      mkdir $DIRECTORY_BSPWM
+        mv -vf .bspwmrc $DIRECTORY_BSPWM/bspwmrc
+  	    chmod +x $HOME/.config/bspwm/bspwmrc
 else
-	rm -vrf $DIRECTORY_BSPWM
+	      rm -vrf $DIRECTORY_BSPWM
         mkdir $DIRECTORY_BSPWM
-        cp -vrf bspwmrc $DIRECTORY_BSPWM/bspwmrc
+        mv -vf .bspwmrc $DIRECTORY_BSPWM/bspwmrc
         chmod +x $HOME/.config/bspwm/bspwmrc
-
 fi
 
 if ! [ -d $DIRECTORY_SXHKD  ]
 then
        	mkdir $DIRECTORY_SXHKD
-       	cp -vf sxhkdrc $DIRECTORY_SXHKD
+       	mv -vf .sxhkdrc $DIRECTORY_SXHKD/sxhkdrc
 else
 	rm -vrf $DIRECTORY_SXHKD
 	mkdir $DIRECTORY_SXHKD
-       	cp -vf sxhkdrc $DIRECTORY_SXHKD
+       	mv -vf .sxhkdrc $DIRECTORY_SXHKD/sxhkdrc
 fi
 
 if ! [ -d $DIRECTORY_POLYBAR  ]
 then
        	mkdir $DIRECTORY_POLYBAR
-       	cp -vrf config.ini launch.sh $DIRECTORY_POLYBAR
-	chmod +x $HOME/.config/polybar/launch.sh
+        mv -vf .config.ini $DIRECTORY_POLYBAR/config.ini
+        mv -vf .launch.sh $DIRECTORY_POLYBAR/launch.sh
+	      chmod +x $HOME/.config/polybar/launch.sh
 else
        	rm -vrf $DIRECTORY_POLYBAR
        	mkdir $DIRECTORY_POLYBAR
-	cp -vrf	config.ini launch.sh $DIRECTORY_POLYBAR
+        mv -vf .config.ini $DIRECTORY_POLYBAR/config.ini
+        mv -vf .launch.sh $DIRECTORY_POLYBAR/launch.sh
         chmod +x $HOME/.config/polybar/launch.sh
 fi
 cp -vf /etc/X11/xinit/xinitrc ~/.xinitrc
-echo 'dbus-run-session bspwm' | tee -a ~/.xinitrc
+echo 'sxhkd &' | tee -a ~/.xinitrc
+echo 'exec bspwm' | tee -a ~/.xinitrc
