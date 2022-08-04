af251804ee88a361020f67809cc35f7ef89f7a54
hecdelatorre <hector982015@gmail.com>
2022-08-03 23:25:07 -0500

    Add jetbrains-mono-fonts-all

--- Fedora/install-fonts.sh
+++ Fedora/install-fonts.sh
@@ -2,4 +2,4 @@
 echo -e "\n Add RPM Fusion provides\n"
 sudo dnf in https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm fedora-workstation-repositories -y
 echo -e "\n Install Fonts\n"
-sudo dnf in adobe-source-code-pro-fonts cascadia-code-fonts fira-code-fonts google-roboto* google-rubik-fonts mozilla-fira* ibm-plex* -y
+sudo dnf in adobe-source-code-pro-fonts cascadia-code-fonts fira-code-fonts google-roboto* google-rubik-fonts mozilla-fira* ibm-plex* jetbrains-mono-fonts-all -y
