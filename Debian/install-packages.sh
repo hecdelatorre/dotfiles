58848aec7f8a911166ba507ce311172765298cbd
hecdelatorre <hector982015@gmail.com>
2022-10-05 00:26:13 -0500

    Edit apps

--- Debian/install-packages.sh
+++ Debian/install-packages.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-sudo apt update && sudo apt install arj audacious build-essential cabextract clang cups-pdf deluge filezilla fonts-firacode g++ gcc gcp git git gnome-disk-utility gparted kitty lhasa lzip lzma lzop mpack neofetch nmap nodejs npm openvpn p7zip p7zip-full python3 python3-pip python3-tk rofi sharutils smplayer tree unace w3m zsh -y
-sudo apt autoremove -y
+#! /bin/bash
+sudo apt install -y lightdm-settings slick-greeter gnome-disk-utility gparted git numlockx curl wget systemd-timesyncd vlc smplayer audacious cifs-utils rtorrent aria2 python3 python3-pip python3-venv python3-tk deluge filezilla fonts-firacode tar p7zip p7zip-full lhasa lzip lzma lzop lzd xz-utils zutils openvpn tree unace w3m zsh coreutils
